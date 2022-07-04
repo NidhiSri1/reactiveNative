8208e66a1c7152b4498f071cfa1549f17f0b917d
NidhiSri1 <srinidhivaja@gmail.com>
2022-07-04 10:49:17 +0530

    commit

--- polling/polling/App.js
+++ polling/polling/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    useEffect(() => {
+        handleRequest();
+    }, []);
+
+    const handleRequest = async () => {
+        try {
+            const data = await axios.get(
+                "https://hn.algolia.com/api/v1/search_by_date?tags=story&page=2"
+            );
+
+            console.log(data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
